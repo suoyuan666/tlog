ca14a67df81ef70d3dbddf147bb150e3ffbe4bb2
suoyuan666 <107405806+suoyuan666@users.noreply.github.com>
2024-08-15 09:02:20 +0800

    add a little test

--- test/main.cpp
+++ test/main.cpp
@@ -1,8 +1,13 @@
 #include <tlog.h>
 
-auto main() -> int { 
-  tlog::tprint({"test", "测试"}, tlog::tlog_status::INFO, tlog::NO_LOG_FILE);  
-  tlog::tprint({"test", "测试"}, tlog::tlog_status::SUCCESS, tlog::NO_LOG_FILE);
+#include <string>
+
+int main(void) {
+  std::string aaa = "abd";
+  // clang-format off
+  tlog::tprint({"test", "测试", aaa}, tlog::tlog_status::INFO, tlog::NO_LOG_FILE);
+  tlog::tprint({}, tlog::tlog_status::INFO, tlog::NO_LOG_FILE);
+  tlog::tprint({"test", "测试", aaa}, tlog::tlog_status::SUCCESS, tlog::NO_LOG_FILE);
   tlog::tprint({"test", "测试"}, tlog::tlog_status::DEBUG, tlog::NO_LOG_FILE);
   tlog::tprint({"test", "测试"}, tlog::tlog_status::WARNNING, tlog::NO_LOG_FILE);
   tlog::tprint({"test", "测试"}, tlog::tlog_status::ERROR, tlog::NO_LOG_FILE);
